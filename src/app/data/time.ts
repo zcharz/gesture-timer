a012a03109c292ae59bb415917a2a02a1cd92e99
Michael DeVera <michael.devera0000@gmail.com>
2023-12-09 15:02:09 -0800

    option set timer

--- src/app/data/time.ts
+++ src/app/data/time.ts
@@ -3,10 +3,19 @@ export class Time {
     public minutes:number;
     public seconds:number;
     public milliseconds: number;
+    public static DEFAULT_TIMES: Time[] = [
+        new Time(0,1,0),new Time(0,2,0),new Time(0,3,0),new Time(0,5,0),new Time(0,10,0),
+        new Time(0,15,0),new Time(0,20,0),new Time(0,30,0),new Time(1,0,0), new Time(2,0,0)];
+
     constructor(h:number, m:number, s:number){
         this.hours = h;
         this.minutes = m;
         this.seconds = s;
         this.milliseconds = 0;
     }
+
+    public toString(){
+        return this.hours + "h " + this.minutes + "m " + this.seconds + "s";
+    }
+
 }
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -12,7 +12,8 @@ export class TimerComponent {
 
   paused:boolean;
   currentTime: Time;
-  initialLength: Time;
+  initialTime: Time;
+  defaultTimesPosition: number;
   //only useful when timer is running
   endsAt: Date;
   startedAt: Date;
@@ -22,25 +23,21 @@ export class TimerComponent {
   constructor(){}
 
   ngOnInit(){
-    this.initialLength = new Time(0,5,0);
+    this.initialTime = new Time(0,5,0);
     this.currentTime = new Time(0,5,0);
     this.paused = true;
+    this.defaultTimesPosition = 3;
     console.log(this.paused);
 
     setInterval(function (this:TimerComponent) {
       this.tick();
-    }.bind(this), 1000)
+    }.bind(this), 500)
   }
-  /*
-  setInterval(function (this:LogSleepPage) {
-    this.curr = new Date();
-  }.bind(this),500)}
-  */
 
   toggleTimer(){
     if(this.paused){ //start timer
       
-      this.endsAt = new Date(Date.now() + this.currentTime.minutes * 60 * 1000);
+      this.endsAt = new Date(Date.now() + (this.currentTime.hours *60*60*1000) + (this.currentTime.minutes * 60 * 1000) + (this.currentTime.seconds * 1000));
       console.log(this.endsAt.toTimeString());
       this.paused = false;
       
@@ -64,8 +61,48 @@ export class TimerComponent {
       this.currentTime = new Time(hours, minutes, seconds);
       
     }
-    
+  }
 
+  reset(){
+    this.paused = true;
+    this.currentTime = this.initialTime;
+  }
+
+  isAtInitialTime(){
+    return this.currentTime.hours == this.initialTime.hours && this.currentTime.minutes == this.initialTime.minutes && this.currentTime.seconds == this.initialTime.seconds
+  }
+  getTimeLeft(){
+    if(this.defaultTimesPosition == 0){
+      return Time.DEFAULT_TIMES[Time.DEFAULT_TIMES.length-1].toString();
+    }
+    return Time.DEFAULT_TIMES[this.defaultTimesPosition-1].toString();
+  }
+
+  setTimeLeft(){
+    this.defaultTimesPosition -= 1;
+    if(this.defaultTimesPosition < 0){
+      this.defaultTimesPosition = Time.DEFAULT_TIMES.length-1;
+    }
+
+    this.initialTime = Time.DEFAULT_TIMES[this.defaultTimesPosition];
+    this.currentTime = Time.DEFAULT_TIMES[this.defaultTimesPosition];
+  }
+
+  getTimeRight(){
+    if(this.defaultTimesPosition == Time.DEFAULT_TIMES.length-1){
+      return Time.DEFAULT_TIMES[0].toString();
+    }
+    return Time.DEFAULT_TIMES[this.defaultTimesPosition+1].toString();
+  }
+
+  setTimeRight(){
+    this.defaultTimesPosition += 1;
+    if(this.defaultTimesPosition > Time.DEFAULT_TIMES.length-1){
+      this.defaultTimesPosition = 0;
+    }
+
+    this.initialTime = Time.DEFAULT_TIMES[this.defaultTimesPosition];
+    this.currentTime = Time.DEFAULT_TIMES[this.defaultTimesPosition];
   }
 
 }
